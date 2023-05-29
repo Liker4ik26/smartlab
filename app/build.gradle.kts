542510e9325ad037245d25165ef978a35052670c
Liker4ik26 <104251391+Liker4ik26@users.noreply.github.com>
2023-05-29 16:04:57 +0600

    added convention, edited file build.gradle.kts(:app), added plugins and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp") version ("1.8.20-1.0.10")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
+    kotlin("kapt")
 }
 
 android {
@@ -33,37 +37,87 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
     }
+
     buildFeatures {
+        buildConfig = true
         compose = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.5"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    ksp {
+        arg("compose-destinations.generateNavGraphs", "false")
+    }
+
+    kapt {
+        correctErrorTypes = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.moshi.kotlin)
+    implementation(libs.moshi.adapters)
+    ksp(libs.moshi.compiler)
+
+    implementation(libs.okhttp.client)
+    implementation(libs.okhttp.logginginterceptor)
+
+    implementation(libs.retrofit.client)
+    implementation(libs.retrofit.moshi)
+
+    implementation(libs.hilt)
+    kapt(libs.hilt.kapt)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+
+    implementation(libs.compose.destinations)
+    implementation(libs.hilt.viewmodel)
+    ksp(libs.compose.destinations.compiler)
+
+    implementation(libs.androidx.splashscreen)
+
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.coil.compose)
+
+    implementation(libs.accompanist.placeholder)
+    implementation(libs.accompanist.switerefreshlayout)
+    implementation(libs.accompanist.systemuicontroller)
+    implementation(libs.accompanist.insets)
+
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.compiler)
+    implementation(libs.moshi.adapters)
+
+    implementation(libs.retrofit.client)
+    implementation(libs.retrofit.moshi)
+
+    implementation(libs.okhttp.client)
+    implementation(libs.okhttp.logginginterceptor)
+
+    implementation(libs.androidx.datastore.preferences)
+
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    debugImplementation(libs.androidx.compose.tooling)
 }
