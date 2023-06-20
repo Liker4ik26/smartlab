beca8e6a4b78a23573de3c12c23411bd890336ed
Liker4ik26 <104251391+Liker4ik26@users.noreply.github.com>
2023-06-20 15:57:20 +0600

    Update NewsItemUi

--- app/src/main/java/com/compose/medicine/smartlab/screens/analyzes/presentation/models/NewsAndStockItemUi.kt
+++ app/src/main/java/com/compose/medicine/smartlab/screens/analyzes/presentation/models/NewsAndStockItemUi.kt
@@ -1,8 +1,9 @@
 package com.compose.medicine.smartlab.screens.analyzes.presentation.models
 
 class NewsAndStockItemUi(
-    val title: String,
+    val id: Int,
+    val name: String,
     val description: String,
     val price: String,
-    val image: Int
+    val image: String
 )
